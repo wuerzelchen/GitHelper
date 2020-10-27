b9589c046c27d825a9f427d88ce84395216fc201
Wolfgang Ladermann <extern.ladermann_wolfgang@allianz.de>
2020-10-27 12:50:05 +0100

    added a fetch to fetch other branches updates

--- refresh_git.sh
+++ refresh_git.sh
@@ -4,6 +4,7 @@ for foldername in */; do \
   cd $foldername && \
   branch=$(git rev-parse --abbrev-ref HEAD) && \
   echo "======= Pulling $foldername --- branch: $branch  =======" && \
+  git fetch && \
   git pull && \
   git remote prune origin 
   cd $BASEDIR
