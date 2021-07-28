ebe30df291e6def860fd3a1fc901fcacf6281fed
Wolfgang Ladermann <wuerzelchen@users.noreply.github.com>
2021-07-28 12:59:22 +0200

    fixed user.name setting

--- set_git_config.sh
+++ set_git_config.sh
@@ -8,6 +8,6 @@ do
 done
 
 for dir in */; do
-	(cd -- "$dir" && git config user.name $username && git config user.email $email && git config user.signingkey $signingkey) 
+	(cd -- "$dir" && git config --replace-all user.name "$username" && git config user.email $email && git config user.signingkey $signingkey) 
 done
 	
